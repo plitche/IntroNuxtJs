413eb7b5194518fcfbfae26e2ab2e62739fc67f0
plitche <kysu728@naver.com>
2022-09-28 23:41:40 +0900

    19. Nuxt의 데이터 호출 방식과 API연동 - axios 설치 및 API 호출

--- Project/learn-nuxt/pages/myMain.vue
+++ Project/learn-nuxt/pages/myMain.vue
@@ -5,7 +5,14 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+  async created() {
+    const response = await axios.get('https://3000/products')
+    console.log(response)
+  }
+
 }
 </script>
 
