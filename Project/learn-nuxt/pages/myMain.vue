9d6a3fe71837f1813db70e5f320d9fb90ac43c4a
plitche <kysu728@naver.com>
2022-09-30 22:48:48 +0900

    23. Nuxt의 데이터 호출 방식과 API연동 - asyncData 속성 안내 및 코드 수정

--- Project/learn-nuxt/pages/myMain.vue
+++ Project/learn-nuxt/pages/myMain.vue
@@ -14,18 +14,19 @@ export default {
   async asyncData() {
     const response = await axios.get('https://3000/products')
     console.log(response)
-    this.products = response.data
+    const products = response.data
+    return { products }
   },
-  data() {
-    return {
-      products: []
-    }
-  },
-  async created() {
-    const response = await axios.get('https://3000/products')
-    console.log(response)
-    this.products = response.data
-  }
+  // data() {
+  //   return {
+  //     products: []
+  //   }
+  // },
+  // async created() {
+  //   const response = await axios.get('https://3000/products')
+  //   console.log(response)
+  //   this.products = response.data
+  // }
 
 }
 </script>
