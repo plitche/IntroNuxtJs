97b9b01587a34e3fe4a6e176b2741d87c0f2a0ba
plitche <kysu728@naver.com>
2022-10-01 15:07:28 +0900

    26. 쇼핑 상품 목록 페이지와 상세 페이지 개발 - 상품 목록 표기 기능 구현

--- Project/learn-nuxt/pages/myMain.vue
+++ Project/learn-nuxt/pages/myMain.vue
@@ -1,24 +1,34 @@
 <template>
   <div>
     <p>메인 페이지입니다.</p>
-    <ProductList></ProductList>
+    <div>
+      <ul>
+        <li v-for="product in products" :key="product.id">
+          <img :src="product.imageUrl" :alt="product.name" />
+          <p>{{ product.name }}</p>
+          <p>{{ product.price }}</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-import ProductList from '~/components/ProjectList.vue'
 
 export default {
-  components: {
-    ProductList
-  },
   async asyncData() {
     const response = await axios.get('https://3000/products')
     console.log(response)
-    const products = response.data
+    const products = response.data.map(item => {
+      return {
+        ...item,
+      imageUrl: `${item.imageUrl}?random=${Math.random()}`
+      }
+    })
     return { products }
   },
+
   // data() {
   //   return {
   //     products: []
