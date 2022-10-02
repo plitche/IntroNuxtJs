32cbfe0d71a087b87b323117ebe32ecf72dca72c
plitche <kysu728@naver.com>
2022-10-02 22:35:31 +0900

    30. 쇼핑 상품 목록 페이지와 상세 페이지 개발 - 상품 상세 정보 조회 기능 개발

--- Project/learn-nuxt/pages/detail/_id.vue
+++ Project/learn-nuxt/pages/detail/_id.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
     <h1>상세 페이지</h1>
+    <div>
+        {{ product }}
+        <img :src="product.imageUrl" :alt="product.name" />>
+        <p>name: {{ product.name }}</p>
+        <p>price: {{ product.price }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import { fetchProductById } from '@/api/index'
+
 export default {
-    created() {
-        console.log(this.$route);
+    // created() {
+    //     const id = this.$route.params.id;
+    //     fetchProductById(id);
+    // }
+
+    async asyncData({ params }) {
+        // const id = params.id;
+        const response = await fetchProductById(params.id);
+        const product = response.data
+        return { product }
     }
 }
 </script>
